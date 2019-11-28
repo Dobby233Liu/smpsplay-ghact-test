3e22bb263f91dc3290edb0369fad83346881d21e
Liu-Wen-Yuan <15816141883@163.com>
2019-11-28 20:32:35 +0800

    Create builder_gcc_x64.bat

---
+++ builder_gcc_x64.bat
@@ -0,0 +1,25 @@
+@echo off
+rem set GNUMAKEFLAGS=-j4 before running the script to speed up the build on multicore systems
+set BUILD_DIR="build_GCC"
+set BASE_DIR="%cd%\download"
+set INST_DIR="%cd%\install"
+set CXXFLAGS="-m64"
+set CFLAGS="-m64"
+
+rem create clean folder structure
+rmdir %BUILD_DIR%
+mkdir %BUILD_DIR%
+cd %BUILD_DIR%
+
+
+rem ---- build/install libvgm ---
+mkdir libvgm
+cd libvgm
+
+set LIBVGM_CMAKE_OPTS="-D CMAKE_BUILD_TYPE=Release -D BUILD_TESTS=OFF -D BUILD_PLAYER=OFF -D BUILD_LIBAUDIO=ON -D BUILD_LIBEMU=ON -D BUILD_LIBPLAYER=OFF -D SNDEMU_ALL=OFF -D SNDEMU_YM2612_GPGX=ON -D SNDEMU_SN76496_MAME=ON -D SNDEMU_UPD7759_ALL=ON"
+cmake -G "MinGW Makefiles" -D CMAKE_INSTALL_PREFIX="%INST_DIR%" %LIBVGM_CMAKE_OPTS% "%BASE_DIR%/libvgm"
+make install
+
+cd ..
+
+echo Done.
